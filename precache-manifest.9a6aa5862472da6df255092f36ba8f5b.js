b5957894c4dd7b6540f1e4efd69520a5493961c0
edray28 <edraytuyor@gmail.com>
2022-03-25 17:30:36 +0800

    Updates

---
+++ precache-manifest.9a6aa5862472da6df255092f36ba8f5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a6e8e1eda013ad55f15",
+    "url": "/my-app/static/css/main.30545427.chunk.css"
+  },
+  {
+    "revision": "6a6e8e1eda013ad55f15",
+    "url": "/my-app/static/js/main.6a6e8e1e.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/my-app/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "8692c13caa49cc47aa92",
+    "url": "/my-app/static/css/2.0b3c5563.chunk.css"
+  },
+  {
+    "revision": "8692c13caa49cc47aa92",
+    "url": "/my-app/static/js/2.8692c13c.chunk.js"
+  },
+  {
+    "revision": "8e74a222f7f961dbd796",
+    "url": "/my-app/static/js/runtime~main.8e74a222.js"
+  },
+  {
+    "revision": "0ad3ad5151a5bbc518ed0ff4f41cd18d",
+    "url": "/my-app/static/media/Doom2016.0ad3ad51.ttf"
+  },
+  {
+    "revision": "52bed81994c01e5320d1a055ae46e417",
+    "url": "/my-app/index.html"
+  }
+];
